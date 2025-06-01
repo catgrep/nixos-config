fe61780cd27cdc5c5c8a60a3c109e0c03dab6f7e
Bobby Dhillon <bdhillon1994@gmail.com>
2025-05-31 18:57:56 -0700

    Reorganize flake files
    Move flake.* -> hosts,
         modules -> hosts
    
    Update home-manager
    
    Update Makefile to reference home-manager flake directory

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -9,14 +9,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."bobby" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./default.nix ];
+        modules = [
+          (builtins.path {
+            path = ./default.nix;
+            name = "home-config";
+          })
+        ];
       };
     };
 }

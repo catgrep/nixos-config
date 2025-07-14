1bea566b6bfa9a62bf9d01336977b4b7749e48cc
Bobby Dhillon <bdhillon1994@gmail.com>
2025-07-13 19:41:03 -0700

    hosts/beelink: Tweak MergerFS config
    The sde and sdf disks are continually spinning. This is a combo of samba
    periodically polling for changes, like metadata, and ext4 journaling.
    
    Use "noatime" to disable access time updates on file reads. This will
    reduce metadata churn.
    
    Configure samba to stop polling the fs for changes.
    
    The 'iostat' command was helpful for diagnosing:
    sudo iostat -xm 2 | tee io.log
    
    And I added a 15 minute spindown timeout for these disks:
    sudo hdparm -S 180 /dev/sde
    sudo hdparm -S 180 /dev/sdf

--- hosts/beelink/disko-config.nix
+++ hosts/beelink/disko-config.nix
@@ -122,7 +122,7 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/mnt/disk1";
-                mountOptions = [ "defaults" "nofail" ];
+                mountOptions = [ "defaults" "nofail" "noatime" ];
               };
             };
           };
@@ -142,7 +142,7 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/mnt/disk2";
-                mountOptions = [ "defaults" "nofail" ];
+                mountOptions = [ "defaults" "nofail" "noatime" ];
               };
             };
           };

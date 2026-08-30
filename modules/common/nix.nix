be5eab4ef1b234b617f4fbb115748eab4186dbac
Bobby Dhillon <bdhillon1994@gmail.com>
2026-08-29 22:40:42 -0700

    hosts: stop auto-upgrading from an unclaimed namespace

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -40,16 +40,4 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
-
-  # Automatic system updates
-  system.autoUpgrade = {
-    enable = true;
-    dates = "04:00";
-    flake = "github:your-username/nixos-config";
-    flags = [
-      "--update-input"
-      "nixpkgs"
-    ];
-    allowReboot = false; # Set to true if you want automatic reboots
-  };
 }
